55ca19ccb07c01561962c4c39b4997f542afc47c
Sheldon Bakker <acum3n@protonmail.com>
2024-12-19 06:24:56 +0200

    Update Tailwindcss.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from '@tailwindcss/forms'
+
 export default {
   content: [
     "./index.html",
@@ -8,6 +10,7 @@ export default {
     extend: {},
   },
   plugins: [
+    plugin,
     require('tailwind-scrollbar'),
   ],
 };
